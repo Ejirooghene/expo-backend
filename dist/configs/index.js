880105be16673a85663a9f6f60bec19941f92093
Greatest <joshuaejiro58@gmail.com>
2024-04-24 01:43:27 +0100

    removed the dist

--- dist/configs/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectDB = void 0;
-var dbConfig_1 = require("./dbConfig");
-Object.defineProperty(exports, "connectDB", { enumerable: true, get: function () { return __importDefault(dbConfig_1).default; } });
